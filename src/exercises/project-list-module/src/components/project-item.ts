dd882a1dfe4c575a64af182b2bfffee24434142c
Angga Ari Wijaya <anggadarkprince@gmail.com>
2022-03-23 15:06:11 +0700

    fix namespace import

--- src/exercises/project-list-module/src/components/project-item.ts
+++ src/exercises/project-list-module/src/components/project-item.ts
@@ -1,4 +1,6 @@
 ///  <reference path="base-component.ts" />
+///  <reference path="../decorators/autobind.ts" />
+///  <reference path="../models/drag-drop.ts" />
 
 namespace App {
     export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
--- src/exercises/project-list-module/src/components/project-list.ts
+++ src/exercises/project-list-module/src/components/project-list.ts
@@ -1,4 +1,8 @@
 ///  <reference path="base-component.ts" />
+///  <reference path="../decorators/autobind.ts" />
+///  <reference path="../states/project-state.ts" />
+///  <reference path="../models/drag-drop.ts" />
+///  <reference path="../models/project.ts" />
 
 namespace App {
     export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
