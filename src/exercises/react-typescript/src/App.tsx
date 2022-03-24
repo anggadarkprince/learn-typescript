d4be84f7824bab8e14f3cc71439c474939abfffe
Angga Ari Wijaya <anggadarkprince@gmail.com>
2022-03-24 12:51:36 +0700

    add react with typescript

---
+++ src/exercises/react-typescript/src/App.tsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import {Todo} from "./todo.model";
+
+const App: React.FC = () => {
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const todoAddHandler = (text: string) => {
+        setTodos(prevTodos => [
+            ...prevTodos,
+            {id: (new Date()).getTime().toString(), text: text}
+        ]);
+    }
+
+    const todoDeleteHandler = (todoId: string) => {
+        setTodos(prevState => {
+            return prevState.filter(todo => todo.id !== todoId)
+        })
+    };
+
+    return (
+        <div className="App">
+            <NewTodo onAddTodo={todoAddHandler}/>
+            <TodoList items={todos} onDeleteTodo={todoDeleteHandler}/>
+        </div>
+    )
+}
+
+export default App;
